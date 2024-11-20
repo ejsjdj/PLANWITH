cc97cdd8a36168200b695f40f4a8a03505fc2386
kgitbank10 <kgitbank10@DESKTOP-LTLNC28>
2024-11-20 16:15:29 +0900

    seohee git test2

--- PLANWITH/src/main/java/com/itbank/PLANWITH/HomeController.java
+++ PLANWITH/src/main/java/com/itbank/PLANWITH/HomeController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	// 20241120
-
 	@RequestMapping("/")
 	public String home() {
 		return "home";
